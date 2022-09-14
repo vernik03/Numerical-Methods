afe65d2820a573324e7b6f71fdb1b43de4c8fab6
vernik03 <vernik03@gmail.com>
2022-09-14 23:37:34 +0300

    add lambdas

--- Finding_the_roots.py
+++ Finding_the_roots.py
@@ -1,6 +1,5 @@
 import math
 
-
 class Calculator:
     def __init__(self, e=0.00001, a=-10, b=10):
         #ax^2 + bx + c = 0
@@ -11,11 +10,11 @@ class Calculator:
         self.e = e
         self.segment = (a,b)
 
-    def f(self, x):
-        return self.a*x**3 + self.b*math.sin(x)*x**2 + self.c
+    f = lambda self, x: self.a*x**3 + self.b*math.sin(x)*x**2 + self.c
 
-    def f_prime(self, x):
-        return self.a*3*x**2 + self.b*math.sin(x)*2*x + self.b*math.cos(x)*x**2
+    f_derivative = lambda self, x: self.a*3*x**2 + self.b*math.sin(x)*2*x + self.b*5*math.cos(x)*x
+    
+    f_derivative_second = lambda self, x: self.a*6*x + self.b*math.sin(x)*(x**2 - 2) + self.b*20*math.cos(x)*x
 
     def dichotomy_method(self):
         a, b = self.segment
@@ -30,8 +29,15 @@ class Calculator:
     def newton_method(self):
         x = self.segment[0]
         while abs(self.f(x)) > self.e:
-            x = x - self.f(x)/self.f_prime(x)
+            x = x - self.f(x)/self.f_derivative(x)
         return x
+
+    # def relaxation_method(self):
+    #     x = self.segment[0]
+    #     while abs(self.f(x)) > self.e:
+    #         x = x - self.f(x)/self.f_derivative_second(x)
+    #     return x
+
 
 
 if __name__ == '__main__':
