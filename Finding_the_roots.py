679d21378ccba22d2fc3e5b119500222afc50762
vernik03 <vernik03@gmail.com>
2022-09-21 17:01:01 +0300

    add iteration and improve relaxation

--- Finding_the_roots.py
+++ Finding_the_roots.py
@@ -1,9 +1,12 @@
+from cmath import sin
 import math
 import struct
 from time import sleep
 import numpy as np
 import matplotlib.pyplot as plt
 import threading
+import warnings
+warnings.filterwarnings("ignore")
 
 class function1:
     def __init__(self):
@@ -21,8 +24,23 @@ class function2:
     f = lambda self, x: x**2 + self.a
     df = lambda self, x: 2*x
 
+class function3:
+    def __init__(self):
+        self.a = -4.0
+
+    f = lambda self, x: x**2 + np.sin(x) - 12.0*x - 0.25
+
+class function4:
+    def __init__(self):
+        self.a = 3.0
+        self.b = 1.0
+        
+    f = lambda self, x: self.a*x + np.cos(x) + self.b
+    df = lambda self, x: self.a + np.sin(x)
+    phi = lambda self, x: np.sin(x)/self.a
+   
 class Calculator:
-    def __init__(self, e=0.00001, segment = (-2.0,1.1), start = 5, func = function1()):
+    def __init__(self, e=0.0001, segment = (-5,0), start = -5, func = function4()):
         self.e = e
 
         self.start = start
@@ -33,6 +51,7 @@ class Calculator:
         self.real_x = np.linspace(self.segment[0], self.segment[1], 100)
         self.real_y = self.func.f(self.real_x)
         self.der_y = self.func.df(self.real_x)
+        self.phi_y = []
 
     def dichotomy_method(self):
         a, b = self.segment
@@ -48,28 +67,44 @@ class Calculator:
 
     def newton_method(self):
         x = self.func.f(self.start)/self.func.df(self.start)
-        while abs(self.func.f(x)) > self.e:
-            x = x - self.func.f(x)/self.func.df(x)
+        x_prev = 0
+        while abs(x_prev - x) > self.e:
+            x_prev = x
+            x -= self.func.f(x)/self.func.df(x)
         return x
 
     def relaxation_method(self):
         x = (self.segment[0] + self.segment[1]) / 2
+        t = -2/(self.der_y.max() + self.der_y.min())
         while abs(self.func.f(x)) > self.e:
-            t = -1/self.func.df(x)
             x += t * self.func.f(x)
         return x
+
+    def iteration_method(self):
+        
+        self.phi_y = self.func.phi(self.real_x)
+        x = (self.segment[0] + self.segment[1]) / 2
+        while abs(self.func.f(x)) > self.e:
+            t = self.func.phi(x)
+            x += t * self.func.f(x)
+        return float(x)
 
     def draw(self):
         plt.plot(self.real_x, self.real_y, label='f(x)')
         plt.plot(self.real_x, self.der_y, label='df(x)')
+        if len(self.phi_y) > 0:
+            plt.plot(self.real_x, self.phi_y, label='phi(x)')
         plt.grid(True)
         plt.show()
 
 
 if __name__ == '__main__':
+    # print((-np.cos(-1.5)-1)/3-(-1.5))
     calculator = Calculator()
     print(calculator.dichotomy_method())
     print(calculator.newton_method())
     print(calculator.relaxation_method())
+    print(calculator.iteration_method())
     calculator.draw()
     
+
