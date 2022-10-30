471f71a5515fa222fbfe669770d54b06b6ee5903
vernik03 <vernik03@gmail.com>
2022-10-30 19:54:55 +0200

    del comments

--- Finding_the_roots.py
+++ Finding_the_roots.py
@@ -1,33 +1,6 @@
-"""PageRank algorithm with explicit number of iterations.
-
-Returns
--------
-ranking of nodes (pages) in the adjacency matrix
-
-"""
-
 import numpy as np
 
 def pagerank(M, num_iterations: int = 100, d: float = 0.85):
-    """PageRank: The trillion dollar algorithm.
-
-    Parameters
-    ----------
-    M : numpy array
-        adjacency matrix where M_i,j represents the link from 'j' to 'i', such that for all 'j'
-        sum(i, M_i,j) = 1
-    num_iterations : int, optional
-        number of iterations, by default 100
-    d : float, optional
-        damping factor, by default 0.85
-
-    Returns
-    -------
-    numpy array
-        a vector of ranks such that v_i is the i-th rank from [0, 1],
-        v sums to 1
-
-    """
     N = M.shape[1]
     v = np.ones(N) / N
     M_hat = (d * M + (1 - d) / N)
@@ -35,16 +8,15 @@ def pagerank(M, num_iterations: int = 100, d: float = 0.85):
         v = v @ M_hat
     return v
 
-
-
 if __name__ == '__main__':
-    M = np.array([[0, 0, 0, 0, 1],
-              [0.5, 0, 0, 0, 0],
-              [0.5, 0, 0, 0, 0],
-              [0, 1, 0.5, 0, 0],
-              [0, 0, 0.5, 1, 0]])
+    # M = np.array([[0, 0, 0, 0, 1],
+    #           [0.5, 0, 0, 0, 0],
+    #           [0.5, 0, 0, 0, 0],
+    #           [0, 1, 0.5, 0, 0],
+    #           [0, 0, 0.5, 1, 0]])
+    M = np.array([[0, 1, 0, 1/3],
+                  [1/2, 0, 1/2, 1/3],
+                  [1/2, 0, 0, 1/3],
+                  [0, 0, 1/3, 0]])
     v = pagerank(M, 100, 0.85)
-    
-    
-    
-
+    print(v)
